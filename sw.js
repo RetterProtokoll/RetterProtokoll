bc3106852a00d041765f7015acbe5efeb59cc60b
RetterProtokoll <m-meusel@hotmail.de>
2026-06-20 13:21:44 +0200

    Update cache name to v15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'retter-protokoll-cache-v14';
+const CACHE_NAME = 'retter-protokoll-cache-v15';
 
 // 'templates.json' wurde hier hinzugefügt, um sie offline verfügbar zu machen
 const ASSETS_TO_CACHE = [
