08c268f71e1335f084f6316a20bf5f62dad73ab8
RetterProtokoll <m-meusel@hotmail.de>
2026-06-25 00:20:28 +0200

    Update CACHE_NAME to v33
    Increment cache version for profile update.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'retter-protokoll-cache-v32'; // Version erhöht für das Profil-Update
+const CACHE_NAME = 'retter-protokoll-cache-v33'; // Version erhöht für das Profil-Update
 
 // 'templates.json' ist hier enthalten, um sie offline verfügbar zu machen
 const ASSETS_TO_CACHE = [
